e3be3744e4e285095c033729bc33be94cd90cc9c
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-28 16:56:55 -0700

    (feat) Add api key and id for google-images

--- keys.js
+++ keys.js
@@ -4,4 +4,6 @@ if (process.env.NODE_ENV !== 'production') {
 
 module.exports = {
   'mongoURI': `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`,
+  'googleImagesKey': process.env.GOOGLE_IMAGES_API_KEY,
+  'googleImagesId': process.env.GOOGLE_IMAGES_ID
 };
