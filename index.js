4183601cc24de666d780ce241adf5244e12f3ddd
Daniel Roberts <BeardOfDan@gmail.com>
2018-03-28 16:57:25 -0700

    (feat) Add routes for searching images and recent searches

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ mongoose.connect(KEYS.mongoURI);
 require('./models/Searches');
 const Searches = mongoose.model('searches');
 
+const GoogleImages = require('google-images');
+const client = new GoogleImages(KEYS.googleImagesId, KEYS.googleImagesKey);
+
 app.get('/', (req, res, next) => {
   res.send('Image Search Abstraction Layer');
 });
 
+app.get('/imagesearch/:searchTerm:offset', (req, res, next) => {
 
+});
 
+app.get('/latest/imagesearch/', (req, res, next) => {
+
+});
 
 app.listen(PORT, () => {
   console.log('Now listening on port ' + PORT);
